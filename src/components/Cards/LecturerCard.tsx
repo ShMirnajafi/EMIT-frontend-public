57a2b32512aa2a8f24f98850a41efd95a535b497
ShMirnajafi <shahriar.mirnajafizade@gmail.com>
2025-02-07 15:35:02 +0330

    add shadow to cards, when its hovered.

--- src/components/Cards/LecturerCard.tsx
+++ src/components/Cards/LecturerCard.tsx
@@ -11,7 +11,7 @@ interface LecturerCard {
 
 function Lecturer(props: LecturerCard) {
     return (
-        <div className="bg-[#0B253A] rounded-xl shadow-lg w-72 overflow-hidden relative m-2.5">
+        <div className="bg-[#0B253A] rounded-xl shadow-lg w-72 overflow-hidden relative m-2.5 hover:shadow-zinc-400 transition-shadow duration-300 ease-in-out">
             <div>
                 <Image
                     alt={props.name}
@@ -19,14 +19,18 @@ function Lecturer(props: LecturerCard) {
                     width={288}
                     height={450}
                 />
-                <div className="absolute my-2 bottom-0 w-[90%] left-1/2 transform -translate-x-1/2 bg-gray-100 px-4 py-3 rounded-lg shadow-md flex flex-col items-center justify-center space-y-1">
+                <div
+                    className="absolute my-2 bottom-0 w-[90%] left-1/2 transform -translate-x-1/2 bg-gray-100 px-4 py-3 rounded-lg shadow-md flex flex-col items-center justify-center space-y-1">
                     <div className="flex items-center justify-around w-full">
-                        <span className="text-[#0B253A] font-bold text-lg">{props.name}</span>
+                        <div>
+                            <span className="text-[#0B253A] font-bold text-lg">{props.name}</span>
+                            <br/>
+                            <span className="text-[#D05353] font-medium">{props.role}</span>
+                        </div>
                         <div className="w-8 h-8">
-                            <AUTIcon />
+                            <AUTIcon/>
                         </div>
                     </div>
-                    <span className="m-1 text-[#D05353] font-medium">{props.role}</span>
                 </div>
             </div>
         </div>
