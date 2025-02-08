918ec37f35e85007f1a00f0294c0e7af8c63cfdc
ShMirnajafi <shahriar.mirnajafizade@gmail.com>
2025-02-08 18:43:52 +0330

    refactor make message card beautiful, based on Mazinani's comments.

--- src/components/Cards/MessageCard.tsx
+++ src/components/Cards/MessageCard.tsx
@@ -12,18 +12,18 @@ const MessageCard: React.FC<MessageCardProps> = ({type, message}) => {
 
     return (
         <div className={`rounded-xl shadow-lg w-full max-w-md border-transparent border-x-8 ${
-            isSuccess ? 'bg-[#EAF8ED] border-x-[#38A169]' : 'bg-[#FEECEC] border-x-[#E53E3E]'
+            isSuccess ? 'bg-[#DAF8E6] border-x-[#22AD5C]' : 'bg-[#FEF3F3] border-x-[#F23030]'
         }`}>
             <div className='p-6'>
                 <div className="flex justify-end items-center">
-                    <p className={`mx-4 text-right text-xl font-bold ${isSuccess ? 'text-[#228B22]' : 'text-[#E53E3E]'}`}>{message}</p>
-                    <div className={`p-2 rounded-lg ${isSuccess ? 'bg-[#38A169]' : 'bg-[#E53E3E]'} text-white`}>
+                    <p className={`mx-4 text-right text-xl font-bold ${isSuccess ? 'text-[#004434]' : 'text-[#BC1C21]'}`}>{message}</p>
+                    <div className={`p-2 rounded-lg ${isSuccess ? 'bg-[#22AD5C]' : 'bg-[#F23030]'} text-white`}>
                         {isSuccess ? <CheckCircleIcon className="w-6 h-6"/> : <XCircleIcon className="w-6 h-6"/>}
                     </div>
                 </div>
                 <div className="mt-10">
                     <Link href="/" passHref
-                          className={`w-36 px-4 py-2 text-white size-12 font-semibold rounded-md ${isSuccess ? 'bg-[#38A169]' : 'bg-[#E53E3E]'} hover:opacity-80`}>
+                          className={`w-36 px-4 py-2 text-white size-12 font-semibold rounded-md ${isSuccess ? 'bg-[#22AD5C]' : 'bg-[#F23030]'} hover:opacity-80`}>
                         بازگشت به صفحه اصلی
                     </Link>
                 </div>
