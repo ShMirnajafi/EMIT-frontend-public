7bd13810cd7c227a347390952d8468440f065677
ShMirnajafi <shahriar.mirnajafizade@gmail.com>
2025-02-08 18:34:36 +0330

    refactor make message card beautiful, based on Mazinani's comments.

--- src/components/Cards/MessageCard.tsx
+++ src/components/Cards/MessageCard.tsx
@@ -11,12 +11,12 @@ const MessageCard: React.FC<MessageCardProps> = ({type, message}) => {
     const isSuccess = type === 'success';
 
     return (
-        <div className={`rounded-xl shadow-lg w-full max-w-md border-transparent border-x-4 ${
+        <div className={`rounded-xl shadow-lg w-full max-w-md border-transparent border-x-8 ${
             isSuccess ? 'bg-[#EAF8ED] border-x-[#38A169]' : 'bg-[#FEECEC] border-x-[#E53E3E]'
         }`}>
             <div className='p-6'>
                 <div className="flex justify-end items-center">
-                    <p className={`mx-8 text-right text-xl font-bold ${isSuccess ? 'text-[#228B22]' : 'text-[#E53E3E]'}`}>{message}</p>
+                    <p className={`mx-4 text-right text-xl font-bold ${isSuccess ? 'text-[#228B22]' : 'text-[#E53E3E]'}`}>{message}</p>
                     <div className={`p-2 rounded-lg ${isSuccess ? 'bg-[#38A169]' : 'bg-[#E53E3E]'} text-white`}>
                         {isSuccess ? <CheckCircleIcon className="w-6 h-6"/> : <XCircleIcon className="w-6 h-6"/>}
                     </div>
