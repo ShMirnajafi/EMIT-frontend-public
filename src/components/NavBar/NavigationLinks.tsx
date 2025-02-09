dfd93768370e7eeded1ce0b05c79050afa52d5ed
ShMirnajafi <shahriar.mirnajafizade@gmail.com>
2025-02-09 05:52:00 +0330

    add scheduling link to navigation bar.

--- src/components/NavBar/NavigationLinks.tsx
+++ src/components/NavBar/NavigationLinks.tsx
@@ -6,7 +6,7 @@ function NavigationLinks() {
         <ul className="hidden md:flex space-x-12 text-gray-300">
             <li><Link href="/" className="text-[#50A4C8] font-semibold">خانه</Link></li>
             <li><Link href="/lecturers" className="hover:text-white">ارائه دهندگان</Link></li>
-            <li><Link href="#" className="hover:text-white">صفحه دوم</Link></li>
+            <li><Link href="/scheduling" className="hover:text-white">زمانبندی</Link></li>
             <li><Link href="#" className="hover:text-white">صفحه سوم</Link></li>
             <li><Link href="#" className="hover:text-white">صفحه چهارم</Link></li>
             <li><Link href="#" className="hover:text-white">صفحه پنجم</Link></li>
--- src/components/ScheduleTimeLine/ScheduleTime.tsx
+++ src/components/ScheduleTimeLine/ScheduleTime.tsx
@@ -10,10 +10,17 @@ interface ScheduleCardProps {
 }
 
 const ScheduleCard: React.FC<ScheduleCardProps> = ({ title, presenter, date, time }) => {
+    const isMostlyEnglish = (text: string): boolean => {
+        const englishChars = text.match(/[a-zA-Z]/g)?.length || 0;
+        return (englishChars / text.length) > 0.1;
+    };
+
+    const isTitlePersian = isMostlyEnglish(title);
+
     return (
         <div className="flex flex-col items-center relative">
             <ChainIcon/>
-            <div className="bg-[#39657E] text-white p-4 rounded-lg shadow-lg w-64 text-center relative">
+            <div className={`bg-[#39657E] text-white p-4 rounded-lg shadow-lg w-64 relative ${isTitlePersian ? 'text-left' : 'text-right'}`}>
                 <h3 className="font-bold text-lg">{title}</h3>
                 <p className="flex mt-4 items-center justify-end text-sm">
                     {presenter} {presenter !== undefined ? <UserIcon className="w-4 h-4 ml-1"/> : <br/>}
